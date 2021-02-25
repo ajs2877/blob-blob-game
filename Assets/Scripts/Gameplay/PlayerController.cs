a544818c240aa0da91ac57c779a3428cc522d859
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-02-24 21:17:22 -0500

    got test stage and test player up but no collisions
    This is using the tilemap system unity already has. See these two tutorials for more info
    https://www.raywenderlich.com/23-introduction-to-the-new-unity-2d-tilemap-system
    https://www.youtube.com/watch?v=mbzXIOKZurA

---
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed = 5f;
+    [SerializeField]
+    private Transform movePoint;
+    [SerializeField]
+    private LayerMask blockingLayerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementAmount = moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementAmount);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                Move(new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0));
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                Move(new Vector3(0, Input.GetAxisRaw("Vertical"), 0));
+            }
+        }
+    }
+
+    private void Move(Vector3 direction)
+    {
+        Vector3 newPosition = movePoint.position + direction;
+        if (!Physics2D.OverlapCircle(newPosition, 0.2f, blockingLayerMask))
+        {
+            movePoint.position = newPosition;
+        }
+    }
+}
