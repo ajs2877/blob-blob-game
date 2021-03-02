e11ac2fb167053867cc4e075960ef76adeb1ccec
nfb1799 <nfb1799@rit.edu>
2021-03-02 10:48:24 -0500

    Blob and boulder movement

---
+++ Assets/Scripts/Gameplay/Boulder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour
+{
+    public GameObject blob;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "blob")
+        {
+            Debug.Log("yes");
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+            transform.position += movement * Time.deltaTime * blob.GetComponent<CubeMovement>().moveSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/CubeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    private Vector3 startPosition;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
