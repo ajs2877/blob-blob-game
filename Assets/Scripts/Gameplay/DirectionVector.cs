7b0c0341132e598384043ac299cf69bce216bdbe
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-08 23:24:50 -0500

    Added switch tile that switches state based on player movement through it

---
+++ Assets/Scripts/Gameplay/DirectionVector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionVector : MonoBehaviour
+{
+    public Vector2 direction;
+    private Vector2 oldPos;
+    
+    
+    void Update()
+    {
+        direction = (Vector2)transform.position - oldPos;
+        oldPos = transform.position;
+    }
+}
---
+++ Assets/Scripts/Gameplay/SwitchTile.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTile : MonoBehaviour
+{
+    [SerializeField]
+    private bool state;
+
+    [SerializeField]
+    private GameObject[] triggerObjects;
+
+    [SerializeField]
+    private Sprite[] sprites;
+
+    // To keep track when to start detection player movement
+    private bool primed = false;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (primed)
+        {
+            // Detects when the blob is exiting and changes state if the blob is going the right direction
+            if (Vector3.Distance(gameObject.transform.position, col.gameObject.transform.position) > 0.65f)
+            {
+                primed = false;
+                DirectionVector directionVector = col.gameObject.GetComponent<DirectionVector>();
+                Vector2 tileDirection = state ? gameObject.transform.up : gameObject.transform.up * -1;
+                if (Vector2.Angle(tileDirection, directionVector.direction) < 45)
+                {
+                    state = !state;
+                    //changes texture to match
+                    spriteRenderer.sprite = sprites[state ? 1 : 0];
+
+                    if (state)
+                    {
+                        SetTriggers(state);
+                    }
+                }
+            }
+        }
+        // Blob enter far enough inward. Read the switch for switching.
+        else if (Vector2.Distance(gameObject.transform.position, col.gameObject.transform.position) < 0.65f)
+        {
+            primed = true;
+        }
+    }
+
+    /**
+     * Iterate over all triggers and changes their states
+     */
+    private void SetTriggers(bool newState)
+    {
+    }
+}
