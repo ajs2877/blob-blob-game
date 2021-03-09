fe043adedd921ae4ccfd1c6333787bb720bfdc96
jf7689 <jf7689@rit.edu>
2021-03-09 16:52:26 -0500

    Game restart working

---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float restartDelay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // restarts after the amount of seconds the restartDelay variable is set to
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    // restarts the game by reloading the current scene
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
