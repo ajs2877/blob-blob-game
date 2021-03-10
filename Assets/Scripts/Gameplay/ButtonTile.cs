9bb65fa15e09ac765eb291943dd4f1bd52c14e23
nfb1799 <nfb1799@rit.edu>
2021-03-09 21:54:12 -0500

    Button/Switch

---
+++ Assets/Scripts/Gameplay/ButtonTile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTile : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] sprites;
+
+    [SerializeField]
+    private bool active = false;
+
+    [SerializeField]
+    private GameObject[] triggerObjects;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        active = !active;
+        spriteRenderer.sprite = sprites[active ? 1 : 0];    }
+}
