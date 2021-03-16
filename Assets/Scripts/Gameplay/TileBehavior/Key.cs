9131fbe60d4bf31f1b5d2ab277234eed6cb245e5
jf7689 <jf7689@rit.edu>
2021-03-16 18:21:41 -0400

    Key following player working
    Not sure how to trigger door only when player has a key and runs into the door.

---
+++ Assets/Scripts/Gameplay/TileBehavior/Key.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : Triggerable
+{
+    [SerializeField]
+    float speed = 4f;
+
+    bool isFollowing;
+    Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if(!isFollowing)
+            {
+                target = other.transform;
+
+                isFollowing = true;
+                //triggered = true;
+            }
+        }
+    }
+
+}
