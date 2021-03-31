c7df1e71ae43ae95a335eeb0c0ed22a1d55a4264
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-31 13:00:12 -0400

    Significantly improved how level selection screen works. Drag scenes themselves into its Stage Selector script in inspector to dynamically generate buttons for scenes.

---
+++ Assets/Scripts/Level/StageSelector.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelector : MonoBehaviour
+{
+    public Utilities.SceneField[] scenes;
+    public GameObject stageHolder;
+    public GameObject stageButtonPrefab;
+    public bool resetStageProgress = false;
+    
+    void Start()
+    {
+        if (resetStageProgress)
+        {
+            StageProgress.ResetProgress();
+        }
+
+        int playerProgress = StageProgress.GetCompletedLevels();
+        for (int index = 0; index < scenes.Length; ++index)
+        {
+            GameObject newButton = Instantiate(stageButtonPrefab, stageHolder.transform.position, stageHolder.transform.rotation);
+            // Set it into panel that holds stage buttons
+            newButton.transform.parent = stageHolder.transform;
+
+            // Sets up all buttons based on scenes assigned
+            newButton.GetComponent<StageButton>().stageName = scenes[index].SceneName;
+            newButton.GetComponentInChildren<Text>().text = (index + 1).ToString();
+
+            // Player progress starts at 1. Not 0 like index.
+            if (index >= playerProgress)
+            {
+                // Disable all stages we have not reached yet
+                newButton.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+}
