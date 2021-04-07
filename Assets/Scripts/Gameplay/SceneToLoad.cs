6ad2b0784cf4e6143a60ef42ebd3f2a7b6cd9288
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-04-06 23:18:51 -0400

    Added pause screen

---
+++ Assets/Scripts/Gameplay/SceneToLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneToLoad : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
