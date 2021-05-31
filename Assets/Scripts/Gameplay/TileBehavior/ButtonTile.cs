c2481cc78683ec2212340386a70b0a5f7531f7fc
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-05-31 16:33:49 -0400

    added timer to buttons

--- Assets/Scripts/Gameplay/TileBehavior/ButtonTile.cs
+++ Assets/Scripts/Gameplay/TileBehavior/ButtonTile.cs
@@ -11,6 +11,11 @@ public class ButtonTile : Triggerable
 
     private AudioSource sound;
 
+    private bool pressed = false;
+    public float timeTillDeactivation;
+    private float internalTimer;
+    public GameObject timerIcon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,17 +23,45 @@ public class ButtonTile : Triggerable
 
         sound = GetComponent<AudioSource>();
     }
-    
+
+    void FixedUpdate()
+    {
+        if (!pressed && timeTillDeactivation > 0 && internalTimer > 0)
+        {
+            float rotationAngle = (Time.fixedDeltaTime / timeTillDeactivation) * -360; // negative so it goes clockwise
+            timerIcon.transform.RotateAround(timerIcon.transform.position, timerIcon.transform.forward, rotationAngle);
+
+            internalTimer -= Time.fixedDeltaTime;
+            if(internalTimer <= 0)
+            {
+                triggered = false;
+                spriteRenderer.sprite = sprites[0];
+                timerIcon.SetActive(false);
+            }
+        }
+    }
+
     void OnTriggerStay2D(Collider2D col)
     {
         if (!triggered) sound.Play();
         triggered = true;
+        pressed = true;
         spriteRenderer.sprite = sprites[1];
+        if (timeTillDeactivation > 0)
+        {
+            internalTimer = timeTillDeactivation;
+            timerIcon.SetActive(true);
+            timerIcon.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
     }
 
     void OnTriggerExit2D(Collider2D col)
     {
-        triggered = false;
-        spriteRenderer.sprite = sprites[0];
+        pressed = false;
+        if (timeTillDeactivation == 0)
+        {
+            triggered = false;
+            spriteRenderer.sprite = sprites[0];
+        }
     }
 }
