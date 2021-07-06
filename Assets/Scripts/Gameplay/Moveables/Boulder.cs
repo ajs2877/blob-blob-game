aa173136f5473a7d327f3f96bf2822c79195f2c5
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2021-07-06 09:27:56 -0400

    Made boulds have new lava texture when on lava

--- Assets/Scripts/Gameplay/Moveables/Boulder.cs
+++ Assets/Scripts/Gameplay/Moveables/Boulder.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Boulder : Moveables
 {
+    [SerializeField]
+    private Sprite[] sprites;
+
+    private int lavaFloorTouching = 0;
+    private SpriteRenderer spriteRenderer;
+
     protected override void Start()
     {
         base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[0];
     }
 
     void Update()
@@ -14,5 +22,30 @@ public class Boulder : Moveables
         UpdateIsMoving();
         NotifyOccupiedTiles();
         UpdateWasMoving();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.tag;
+        GameObject parent = col.transform.parent.gameObject;
+        if (col.CompareTag("Lava"))
+        {
+            lavaFloorTouching++;
+            spriteRenderer.sprite = sprites[1];
+            spriteRenderer.color = new Color(220, 175, 140);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Lava"))
+        {
+            lavaFloorTouching--;
+            if (lavaFloorTouching == 0)
+            {
+                spriteRenderer.sprite = sprites[0];
+                spriteRenderer.color = new Color(176, 149, 128);
+            }
+        }
     }
 }
