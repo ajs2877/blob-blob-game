6290ea4ce0657b700cb56ba419b1a0cdeee6fa64
nfb1799 <nfb1799@rit.edu>
2021-07-31 10:07:20 -0400

    Volume Slider

---
+++ Assets/Scripts/Audio/VolumeSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField]
+    Slider volumeSlider;
+
+    [SerializeField]
+    AudioSource sound1;
+
+    [SerializeField]
+    AudioSource sound2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = sound1.volume;
+    }
+
+    public void ChangeVolume()
+    {
+        sound1.volume = volumeSlider.value;
+        sound2.volume = volumeSlider.value;
+    }
+}
