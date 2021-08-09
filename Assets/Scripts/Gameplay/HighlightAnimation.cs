490263461a903d95fd852bacc48c78428fce53f6
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2021-08-08 23:25:15 -0400

    Fixed animation of blob indication

--- Assets/Scripts/Gameplay/HighlightAnimation.cs
+++ Assets/Scripts/Gameplay/HighlightAnimation.cs
@@ -9,6 +9,7 @@ public class HighlightAnimation : MonoBehaviour
     public float scaleRange = 0.1f;
     public float transformChangeRate = 1.5f;
     public float transformRange = 0.0f;
+    private float time = 0;
 
     private void Start()
     {
@@ -17,14 +18,17 @@ public class HighlightAnimation : MonoBehaviour
 
     void Update()
     {
-        float sinVal1 = Mathf.Sin(Time.time * scaleChangeRate);
+        // Game is paused and so, pause animation
+        time += Time.deltaTime * Time.timeScale;
+
+        float sinVal1 = Mathf.Sin(time * scaleChangeRate);
         float scaleMultiplier = sinVal1 * scaleRange + 1;
         transform.localScale = startScale * scaleMultiplier;
 
         if(transformRange != 0)
         {
-            float sinVal2 = Mathf.Sin(Time.time * transformChangeRate + 45);
-            float transformMultiplier = sinVal2 * transformRange;
+            float sinVal2 = Mathf.Sin(time * transformChangeRate + 45);
+            float transformMultiplier = sinVal2 * transformRange * Time.timeScale;
             transform.localPosition += new Vector3(0, transformMultiplier, 0);
         }
     }
